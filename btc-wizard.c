a63e82153236d626e2d9075a57efab64330a3810
installer <installer@gmail.com>
2025-09-26 22:46:07 +1000

    version 0.1

---
+++ btc-wizard.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+
+struct variables {
+  CURL *curl;
+  CURLcode res;
+  FILE *fptr;
+  FILE *output;
+  char buffer[1024];
+};
+
+int get_value() {
+  struct variables env;
+  env.curl = curl_easy_init();
+  if(env.curl) {
+    env.fptr = fopen("data.txt", "w");
+    curl_easy_setopt(env.curl, CURLOPT_URL, "https://www.google.com/finance/quote/BHP:ASX");
+    curl_easy_setopt(env.curl, CURLOPT_WRITEDATA, env.fptr);
+    env.res = curl_easy_perform(env.curl);
+    fclose(env.fptr);
+    curl_easy_cleanup(env.curl);
+  }
+}
+
+int main() {
+  get_value();
+}
