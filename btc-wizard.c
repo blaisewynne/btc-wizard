0f5f9345582de2c6466d67d3575c96b7d1370f1e
installer <installer@gmail.com>
2025-09-27 11:15:39 +1000

    version 0.2.1

--- btc-wizard.c
+++ btc-wizard.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <curl/curl.h>
-
+#include <stdbool.h>
 struct variables {
   CURL *curl;
   CURLcode res;
   FILE *fptr;
   FILE *output;
   char buffer[1024];
+  char *end;
 };
 
 int get_value() {
@@ -15,7 +16,7 @@ int get_value() {
   env.curl = curl_easy_init();
   if(env.curl) {
     env.fptr = fopen("data.txt", "w");
-    curl_easy_setopt(env.curl, CURLOPT_URL, "https://www.google.com/finance/quote/BHP:ASX");
+    curl_easy_setopt(env.curl, CURLOPT_URL, "https://www.google.com/finance/quote/BTC-AUD");
     curl_easy_setopt(env.curl, CURLOPT_WRITEDATA, env.fptr);
     env.res = curl_easy_perform(env.curl);
     fclose(env.fptr);
@@ -23,11 +24,23 @@ int get_value() {
   }
 }
 
+char remove_comma(char * str, char c) {
+  char *pr = str, *pw = str;
+  while (*pr) {
+    *pw = *pr++;
+    pw += (*pw != c);
+  }
+  *pw = '\0';
+}
+
 int main() {
   struct variables env;
+  while (true) {
   get_value();
   env.output = popen("grep -Po '(?<=<div class=\"YMlKec fxKbKc\">)(.*?)(?=</div>)' data.txt", "r");
   while (fgets(env.buffer, sizeof(env.buffer), env.output) != NULL) {
+    remove_comma(env.buffer, ',');
     printf("%s", env.buffer);
+  }
   }
 }
